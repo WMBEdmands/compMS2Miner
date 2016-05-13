b8eadb43d9f6ab8168c02cb63eaae170ec0078f0
WMBEdmands <edmandsw@berkeley.edu>
2016-05-13 13:04:03 -0700

    minor change PMIDsearch

--- R/PMIDsearch.R
+++ R/PMIDsearch.R
@@ -1,9 +1,8 @@
 #' customized PMID search function adapted from PubChemWordcloud package v 0.3.2
 #' @param keys character vector of compound names to search pubmed with
 #' @param n numeric maximum number of results to return
-PMIDsearch <- function (keys=NULL,n=1000) 
-{
-  searchUrl<-paste0('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term="',
+PMIDsearch <- function (keys=NULL, n=1000){
+  searchUrl <- paste0('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term="',
                     gsub(" ", "+", keys), paste0('"&retmax=', n))
   hlpURL <- RCurl::getURL(searchUrl)
   doc <- XML::xmlTreeParse(hlpURL, asText = TRUE)
@@ -11,4 +10,4 @@ PMIDsearch <- function (keys=NULL,n=1000)
   Count <- unlist(doc[["doc"]][["eSearchResult"]][["Count"]])[3]
   Idlist <- c(Count, IdlistHlp[grep("value$", names(IdlistHlp))])
   return(Idlist)
-}
+} # end function
