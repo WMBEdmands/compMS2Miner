b14152fa2e537689c5ecaf862f1b4e248235af2c
WMBEdmands <edmandsw@berkeley.edu>
2016-05-18 18:21:24 -0700

    updated publishApp to write local .zip files and also modification of runGitHub shiny and devtools. v1.0.9

--- R/publishApp.R
+++ R/publishApp.R
@@ -6,10 +6,11 @@
 #' Following this the app can be deployed on your account
 #' @param object a compMS2 class object 
 #' @param appName character name for your new app (e.g. 'compMS2example').
-#'  
+#' @param writeDir character full path to a directory to save the compMS2 results
+#' and the current shiny app to. This zip file can then be shared with others.
 #' @param ... further arguments to the \code{\link{deployApp}} function
 #' @export
-setGeneric("publishApp", function(object, appName=NULL, ...) standardGeneric("publishApp"))
+setGeneric("publishApp", function(object, appName=NULL, writeDir=NULL, ...) standardGeneric("publishApp"))
 
 setMethod("publishApp", signature = "CompMS2", function(object, appName=NULL, ...){
   # error handling
@@ -32,8 +33,45 @@ setMethod("publishApp", signature = "CompMS2", function(object, appName=NULL, ..
     }
 
     save(object, file=paste0(appDir, '/compMS2object.RData'))  
+    
+    if(!is.null(writeDir)){
+    # create directory to bundle app in
+    appDirWrite <- paste0(writeDir, '/', appName)
+    dir.create(appDirWrite)
+    # id shiny files and .RData
+    shinyFilesTmp <- list.files(appDir, full.names = T)
+    destZipFile <- paste0(appDirWrite, '/', appName, '.zip')
+    fileAlreadyExists <- file.exists(destZipFile)
+    if(fileAlreadyExists == T){
+      message('destination .zip file:\n\n', destZipFile, '\n\nalready exists...\nDo you wish to overwrite?\n')
+      flush.console()
+      ovWrite <- readline(prompt="Y/N: ")
+      if(grepl('Y', ovWrite, ignore.case = T)){
+        tmpWd <- getwd()
+        setwd(dirname(shinyFilesTmp))
+        fileZipLog <- zip(destZipFile, basename(shinyFilesTmp))
+        setwd(tmpWd)
+      } else {
+        message('stopping...\n')
+        flush.console()
+        opt <- options(show.error.messages=FALSE) 
+        on.exit(options(opt)) 
+        stop() 
+      }
+    } else {
+    tmpWd <- getwd()
+    setwd(dirname(shinyFilesTmp))
+    fileZipLog <- zip(destZipFile, basename(shinyFilesTmp))
+    setwd(tmpWd)
+    }
+    
+    # # packrat shiny app
+    # packrat::init(appDirWrite) 
+    # 
+    } else {
     # deploy app
     shinyapps::deployApp(appDir = appDir, appName=appName, ...)
+    }
    }
 }) # end function
     
---
+++ R/runGitHubApp.R
@@ -0,0 +1,37 @@
+#' run github shiny modified from shiny and devtools
+#' @param repo character github username and repository name. in the form "username/repositoryName"
+#' @param subdir character sub-directory of the repo containing the shiny and data.
+#' @param auth_token character private repo authorization token. 
+#' @export
+#' 
+runGitHubApp <- function(repo=NULL, subdir=NULL, auth_token=NULL){
+  # error handling
+  stopifnot(is.character(repo))
+  stopifnot(is.character(subdir))
+  
+    res <- strsplit(repo, "/")[[1]]
+    if(length(res) != 2){ 
+      stop("'repo' must be of the form 'username/repo'")
+    }
+    username <- res[1]
+    repo <- res[2]
+  
+  remote <- devtools:::remote("github", host = "api.github.com", repo = repo, subdir =
+                              subdir, username = username, ref = NULL, sha = NULL, 
+                              auth_token = auth_token)
+  
+  bundle <- devtools:::remote_download(remote, quiet = F)
+  on.exit(unlink(bundle), add = TRUE)
+  outdir <- tempfile(pattern = "CompMS2miner")
+  dir.create(outdir)
+  
+  pathTmp <- utils::unzip(bundle, exdir = outdir) 
+  
+  tmpIndx <- grepl(paste0('/', subdir, '/'), pathTmp)
+  if(!any(tmpIndx)){
+    stop(subdir, ' sub-directory name not found please check and try again...')
+  }
+  tmpAppDir <- tempfile(pattern='CompMS2miner')
+  appPathTmp <- utils::unzip(pathTmp[tmpIndx], exdir=tmpAppDir)
+  shiny::runApp(dirname(appPathTmp[1]))
+} # end function
