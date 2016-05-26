b9107da10b440e372c0a5dfb46d047dd9ddfb28a
WMBEdmands <edmandsw@berkeley.edu>
2016-05-26 14:51:22 -0700

    added dirPath option to runGitHub to save zip file contents.

--- R/runGitHubApp.R
+++ R/runGitHubApp.R
@@ -1,11 +1,12 @@
 #' run github shiny modified from shiny and devtools
 #' @param repo character github username and repository name. in the form "username/repositoryName"
 #' @param subdir character sub-directory of the repo containing the shiny and data.
+#' @param dirPath character full-path to a directory in which to save the contents of the zip file. If unsupplied shiny app will be opened from a temporary directory. 
 #' @param auth_token character private repo authorization token. 
 #' @param browserLaunch logical launch app in web browser (default = TRUE).
 #'
 #' @export
-runGitHubApp <- function(repo=NULL, subdir=NULL, auth_token=NULL, browserLaunch=TRUE){
+runGitHubApp <- function(repo=NULL, subdir=NULL, dirPath=NULL, auth_token=NULL, browserLaunch=TRUE){
   # error handling
   stopifnot(is.character(repo))
   
@@ -41,6 +42,6 @@ runGitHubApp <- function(repo=NULL, subdir=NULL, auth_token=NULL, browserLaunch=
     stop(subdir, ' sub-directory name not found please check and try again...')
   }
   tmpAppDir <- tempfile(pattern='CompMS2miner')
-  appPathTmp <- utils::unzip(pathTmp[tmpIndx], exdir=tmpAppDir)
+  appPathTmp <- utils::unzip(pathTmp[tmpIndx], exdir=ifelse(is.null(dirPath), tmpAppDir, dirPath))
   shiny::runApp(dirname(appPathTmp[1]), launch.browser = browserLaunch)
 } # end function
