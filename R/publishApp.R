12a9dd5722272cfedffaa792971c88cf448f31d1
WMBEdmands <edmandsw@berkeley.edu>
2016-05-31 16:54:56 -0700

    changes to publish app, everything temp now. v.1.1.7

--- R/publishApp.R
+++ R/publishApp.R
@@ -7,7 +7,7 @@
 #' @param object a compMS2 class object 
 #' @param appName character name for your new app (e.g. 'compMS2example').
 #' @param writeDir character full path to a directory to save the compMS2 results
-#' and the current shiny app to. This zip file can then be shared with others.
+#' and the current shiny app zip file to. This zip file can then be shared with others.if this argument is not supplied the results will be deployed on shinyapps.io.
 #' @param addFiles character vector of full paths to files which will be included
 #' in the zip file or bundle to shinyapps.io. For example code used to generate 
 #' compMS2 results.
@@ -32,29 +32,38 @@ setMethod("publishApp", signature = "CompMS2", function(object, appName=NULL,  w
     
     appDir <- system.file("shiny-apps", "compMS2explorer", package = "CompMS2miner")
     if(appDir == ""){
-      stop('Could not find example directory. Try re-installing "CompMS2miner".', call. = FALSE)
+      stop('Could not find shiny-apps directory. Try re-installing "CompMS2miner".', call. = FALSE)
     }
+    
+    # create temporary directory to create zip
+    outDir <- tempfile(pattern = "CompMS2miner")
+    dir.create(outDir)
     
     if(is.character(addFiles)){
     message('moving the following additional files to the application bundle:\n',
             paste0(basename(addFiles), '\n'))
-    flush.console
-    filesMoved <- file.copy(addFiles, appDir, overwrite = T)
+    flush.console()
+    filesMoved <- file.copy(addFiles, outDir, overwrite = T)
     if(any(filesMoved == F)){
     stop('The file(s):\n', paste0(addFiles[filesMoved == F], '\n'), 'were not copied to the bundle please check the path is correct.\n')
     }
     }  
+    # copy latest version of shiny app from package
+    filesMoved <- file.copy(paste0(appDir, c('/server.R', '/ui.R', '/global.R')), outDir, overwrite = T)
+    if(any(filesMoved == F)){
+      stop('The shiny-app file(s) were not copied to the bundle please check the CompMS2miner package is properly installed.\n')
+    }
     
     if(!is.null(writeDir)){
     # add readOnly = F to parameters when zipped so collaborators can create their own edits
     Parameters(object)$readOnly <- FALSE
-    save(object, file=paste0(appDir, '/compMS2object.RData'))  
-      
-    # create directory to bundle app in
+    save(object, file=paste0(outDir, '/compMS2object.RData'))  
+    
+    # create directory to bundle app and create zip in
     appDirWrite <- paste0(writeDir, '/', appName)
     dir.create(appDirWrite)
     # id shiny files and .RData
-    shinyFilesTmp <- list.files(appDir, full.names = T)
+    shinyFilesTmp <- list.files(outDir, full.names = T)
     # remove the dreaded desktop.ini
     shinyFilesTmp <- shinyFilesTmp[grepl('desktop\\.ini$', shinyFilesTmp) == F]
     destZipFile <- paste0(appDirWrite, '/', appName, '.zip')
@@ -88,10 +97,10 @@ setMethod("publishApp", signature = "CompMS2", function(object, appName=NULL,  w
     } else {
     # add readOnly = TRUE to parameters when published so viewers cannot edit
     Parameters(object)$readOnly <- TRUE
-    save(object, file=paste0(appDir, '/compMS2object.RData'))  
+    save(object, file=paste0(outDir, '/compMS2object.RData'))  
       
     # deploy app
-    shinyapps::deployApp(appDir = appDir, appName=appName, ...)
+    shinyapps::deployApp(appDir = outDir, appName=appName, ...)
     }
    }
 }) # end function
